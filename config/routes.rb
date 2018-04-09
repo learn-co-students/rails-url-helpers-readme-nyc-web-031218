f9542dc38ce8be8b37a956d751fbb3108888388c
harim1206 <harim1206@gmail.com>
2018-04-08 23:13:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show]
+
+
+  get '/posts', to:'posts#index'
+  # get '/posts/:id', to:'posts#show'
+
+  resources :posts   #, only: [:index, :show]
+
+
+
+
+
+
 end
